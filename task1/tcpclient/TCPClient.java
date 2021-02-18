20108b5decf4069f8f50d51e1d6fc39e6713cfe5
spof <spof@kth.se>
2021-02-18 08:26:51 +0100

    first commit

---
+++ task1/tcpclient/TCPClient.java
@@ -0,0 +1,15 @@
+package tcpclient;
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    
+    public static String askServer(String hostname, int port, String ToServer) throws IOException {
+        return null;
+    }
+
+    public static String askServer(String hostname, int port) throws IOException {
+        return null;
+    }
+}
+
